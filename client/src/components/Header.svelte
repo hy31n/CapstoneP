25adaa90e6d2c9a5e6516da79114f56c24fad675
hy31n <he31n@naver.com>
2025-06-10 21:21:27 +0900

    header logo

--- client/src/components/Header.svelte
+++ client/src/components/Header.svelte
@@ -2,6 +2,21 @@
     export let headerName; 
 
 </script>
-<div>
-    {headerName}
+
+<style>
+  .header-container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 40px 20px 0px 0px;
+    text-align: left;
+  }
+  
+  .header-logo {
+    max-height:130px;
+    width: auto;
+  }
+</style>
+
+<div class="header-container">
+        <img src={headerName} alt="Logo" class="header-logo" />
 </div>
