113c8536b7086a15afd504258f65030f2fc43adc
hy31n <he31n@naver.com>
2025-06-10 20:13:34 +0900

    layout

---
+++ client/src/routes/_module.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { headerName } from '../store';
+    import Header from '../components/Header.svelte';
+
+    let headerValue;
+    headerName.subscribe((data) => {
+        headerValue = data;
+    })
+</script>
+
+
+<div class="ui">
+    <Header headerName={headerValue} />
+    <slot />
+</div>
+
